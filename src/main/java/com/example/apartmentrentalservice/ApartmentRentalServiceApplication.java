bc27cac906a7a856182389e9059ad03dbfafa318
unknown <aleksanderinglot@hotmail.comm>
2023-06-09 13:32:58 +0200

    Initialize Spring Boot project using Spring Initializr

---
+++ src/main/java/com/example/apartmentrentalservice/ApartmentRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentRentalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apartmentrentalservice/ApartmentRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentrentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
