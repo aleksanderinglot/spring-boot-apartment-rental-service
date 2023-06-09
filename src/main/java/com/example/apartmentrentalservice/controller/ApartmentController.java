efcece2d231a81bf20f7765ad1a375853ed13ec6
unknown <aleksanderinglot@hotmail.comm>
2023-06-09 22:21:09 +0200

    Create controllers for handling API endpoints

--- src/main/java/com/example/apartmentrentalservice/controller/ApartmentController.java
+++ src/main/java/com/example/apartmentrentalservice/controller/ApartmentController.java
@@ -1,4 +1,86 @@
 package com.example.apartmentrentalservice.controller;
 
+import com.example.apartmentrentalservice.dto.ApartmentDTO;
+import com.example.apartmentrentalservice.model.Apartment;
+import com.example.apartmentrentalservice.service.ApartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/apartments")
+@CrossOrigin(origins = "*")
 public class ApartmentController {
+
+    private final ApartmentService apartmentService;
+
+    @Autowired
+    public ApartmentController(ApartmentService apartmentService) {
+        this.apartmentService = apartmentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ApartmentDTO>> getAllApartments() {
+        List<Apartment> apartments = apartmentService.getAllApartments();
+
+        if (!apartments.isEmpty()) {
+            List<ApartmentDTO> apartmentDTOs = apartmentService.convertToDTOList(apartments);
+            return ResponseEntity.ok(apartmentDTOs);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApartmentDTO> getApartmentById(@PathVariable Long id) {
+        Apartment apartment = apartmentService.getApartmentById(id);
+
+        if (apartment != null) {
+            ApartmentDTO apartmentDTO = apartmentService.convertToDTO(apartment);
+            return ResponseEntity.ok(apartmentDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ApartmentDTO> createApartment(@RequestBody ApartmentDTO apartmentDTO) {
+        Apartment apartment = apartmentService.convertToEntity(apartmentDTO);
+        Apartment savedApartment = apartmentService.createApartment(apartment);
+
+        ApartmentDTO savedApartmentDTO = apartmentService.convertToDTO(savedApartment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedApartmentDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApartmentDTO> updateApartment(@PathVariable Long id, @RequestBody ApartmentDTO apartmentDTO) {
+        Apartment existingApartment = apartmentService.getApartmentById(id);
+
+        if (existingApartment != null) {
+            Apartment updatedApartment = apartmentService.convertToEntity(apartmentDTO);
+            updatedApartment.setId(existingApartment.getId());
+
+            Apartment savedApartment = apartmentService.updateApartment(id, updatedApartment);
+
+            ApartmentDTO savedApartmentDTO = apartmentService.convertToDTO(savedApartment);
+            return ResponseEntity.ok(savedApartmentDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteApartment(@PathVariable Long id) {
+        Apartment existingApartment = apartmentService.getApartmentById(id);
+
+        if (existingApartment != null) {
+            apartmentService.deleteApartment(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/example/apartmentrentalservice/controller/ReservationController.java
+++ src/main/java/com/example/apartmentrentalservice/controller/ReservationController.java
@@ -1,4 +1,107 @@
 package com.example.apartmentrentalservice.controller;
 
+import com.example.apartmentrentalservice.dto.ReservationDTO;
+import com.example.apartmentrentalservice.model.Reservation;
+import com.example.apartmentrentalservice.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationDTO>> getAllReservations() {
+        List<Reservation> reservations = reservationService.getAllReservations();
+
+        if (!reservations.isEmpty()) {
+            List<ReservationDTO> reservationDTOs = reservationService.convertToDTOList(reservations);
+            return ResponseEntity.ok(reservationDTOs);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservationDTO> getReservationById(@PathVariable Long id) {
+        Reservation reservation = reservationService.getReservationById(id);
+
+        if (reservation != null) {
+            ReservationDTO reservationDTO = reservationService.convertToDTO(reservation);
+            return ResponseEntity.ok(reservationDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/user/{username}")
+    public ResponseEntity<List<ReservationDTO>> getUserReservations(@PathVariable String username) {
+        List<Reservation> reservations = reservationService.getUserReservations(username);
+
+        if (!reservations.isEmpty()) {
+            List<ReservationDTO> reservationDTOs = reservationService.convertToDTOList(reservations);
+            return ResponseEntity.ok(reservationDTOs);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationDTO> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        Reservation reservation = reservationService.convertToEntity(reservationDTO);
+
+        boolean isOverlap = reservationService.checkReservationOverlap(reservation);
+        if (isOverlap) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
+
+        Reservation savedReservation = reservationService.createReservation(reservation);
+        ReservationDTO savedReservationDTO = reservationService.convertToDTO(savedReservation);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedReservationDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReservationDTO> updateReservation(@PathVariable Long id, @RequestBody ReservationDTO reservationDTO) {
+        Reservation existingReservation = reservationService.getReservationById(id);
+
+        if (existingReservation != null) {
+            Reservation updatedReservation = reservationService.convertToEntity(reservationDTO);
+            updatedReservation.setId(existingReservation.getId());
+
+            boolean isOverlap = reservationService.checkReservationOverlapForUpdate(updatedReservation, existingReservation);
+            if (isOverlap) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            }
+
+            Reservation savedReservation = reservationService.updateReservation(id, updatedReservation);
+            ReservationDTO savedReservationDTO = reservationService.convertToDTO(savedReservation);
+            return ResponseEntity.ok(savedReservationDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        Reservation existingReservation = reservationService.getReservationById(id);
+
+        if (existingReservation != null) {
+            reservationService.deleteReservation(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
