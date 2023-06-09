a11b9a0f16569130b378bdad644f7ab9d291d635
unknown <aleksanderinglot@hotmail.comm>
2023-06-09 14:27:42 +0200

    Implement model entities

--- src/main/java/com/example/apartmentrentalservice/model/Apartment.java
+++ src/main/java/com/example/apartmentrentalservice/model/Apartment.java
@@ -1,4 +1,26 @@
 package com.example.apartmentrentalservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Apartment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private int numberOfRooms;
+    private double price;
 }
--- src/main/java/com/example/apartmentrentalservice/model/Reservation.java
+++ src/main/java/com/example/apartmentrentalservice/model/Reservation.java
@@ -1,4 +1,26 @@
 package com.example.apartmentrentalservice.model;
 
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reservation")
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long apartmentId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String guestName;
+    private String guestEmail;
 }
